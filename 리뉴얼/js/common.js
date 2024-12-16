b7e05661f338339c5df029c6f13f263360dec722
gawkyerim <jh19930@naver.com>
2024-12-16 13:55:53 +0900

    seventh-portfolio

--- 리뉴얼/js/common.js
+++ 리뉴얼/js/common.js
@@ -62,3 +62,45 @@ closeBtn.addEventListener('click', function() {
 modalOverlay.addEventListener('click', function() {
     modalOverlay.classList.remove('active');
 })
+
+const designList = document.querySelectorAll('.design-list');
+// 디자인 배너 리스트중 하나를 선택하면 
+// 거기 안에있는 이미지가 모달에 있는 컨텐츠 이미지로 들어가야돼!
+
+// 0. 배너 리스트 하나하나 마다를 클릭한다 (addEventListener) (click event)
+designList.forEach(function(banner){
+    banner.addEventListener('click', function() {
+        const bannerImg = this.querySelector('img');
+        // 2. 디자인 배너 안에 있는 이미지 태그 src 주소를 저장해서
+        const modalImg = document.querySelector('.content-design .modal-content img');
+        // 3. 모달안에 컨텐츠 안에 이미지 태그의 src 주소에 할당한다.
+        modalImg.src = bannerImg.src;
+
+        document.querySelector('.content-design .modal').classList.add('on');
+    })
+})
+
+document.querySelector('.content-design .modal').addEventListener('click', function() {
+    this.classList.remove('on');
+})
+
+const tabList = document.querySelectorAll('.content-site .tab li');
+const siteList = document.querySelectorAll('.content-site .view .list-item');
+
+//1.tab > li 의 인덱스 번호를 찾는다.
+tabList.forEach(function(tab_btn, index){
+    //2. tab > li 메뉴들 중 하나를 누른다.
+    tab_btn.addEventListener('click', function() {
+        //3.tab > li 모든 li에 들어간 active를 삭제한다.
+        tabList.forEach(function(tab_other) {
+            tab_other.classList.remove('active');
+        });
+        //4.모든 list-item에 들어간 active를 삭제한다.
+        siteList.forEach(function(site_other) {
+            site_other.classList.remove('active');
+        })
+        //5. 클릭한 요소에만 active를 추가한다.
+        tabList[index].classList.add('active');
+        siteList[index].classList.add('active');
+    });
+});
